3d5eb9d8da4add9ad9b5b1196ac7fb2d75f2b845
yovanraju <yovannainggolan101104@gmail.com>
2024-09-11 14:42:25 +0700

    selesai

---
+++ main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+
+class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    mood = models.CharField(max_length=255)
+    time = models.DateField(auto_now_add=True)
+    feelings = models.TextField()
+    mood_intensity = models.IntegerField()
+
+    @property
+    def is_mood_strong(self):
+        return self.mood_intensity > 5
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://yovan-raju-mentalhealthtracker2.pbp.cs.ui.ac.id/", "https://yovan-raju-mentalhealthtracker2.pbp.cs.ui.ac.id/"]
