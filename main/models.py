b9cec5d3722f07d795bbbe26b2bd4ebb10698da1
yovanraju <yovannainggolan101104@gmail.com>
2024-09-18 13:59:11 +0700

    tutorial 3

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 class MoodEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0xapcs$0$g_@$k@+3c$en*k_syld6od5bttf*pt2r*9feq)$b^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
@@ -123,4 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://yovan-raju-mentalhealthtracker2.pbp.cs.ui.ac.id/", "https://yovan-raju-mentalhealthtracker2.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://yovan-raju-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://yovan-raju-mentalhealthtracker2.pbp.cs.ui.ac.id/"]
